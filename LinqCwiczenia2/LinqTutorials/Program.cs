a2592af3638cd46829ff60a7307e969f97e8c3fe
MateuszKorba <mkorba90@gmail.com>
2021-07-13 15:30:48 +0200

    Add new files

---
+++ LinqCwiczenia2/LinqTutorials/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace LinqTutorials
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //var t = LinqTasks.Task1();
+            //var t = LinqTasks.Task2(); 
+            //var t = LinqTasks.Task3(); 
+            //var t = LinqTasks.Task4(); 
+            //var t = LinqTasks.Task5(); 
+            //var t = LinqTasks.Task6(); 
+            //var t = LinqTasks.Task7(); 
+            //var t = LinqTasks.Task8(); 
+            //var t = LinqTasks.Task9(); 
+            //var t = LinqTasks.Task10(); 
+            //var t = LinqTasks.Task11(); 
+            var t = LinqTasks.Task12(); 
+
+            //int[] tab = {1,1,1,1,1,1,10,1,1,1,1};
+            //var t = LinqTasks.Task13(tab); 
+
+            //var t = LinqTasks.Task14();
+        }
+    }
+}
